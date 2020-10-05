2909de82763ddad785ac996b29b740e0a887a5c4
haozhigangvip <haozhigang@targetmol.com>
2020-10-06 00:56:20 +0800

    123

--- account/src/main/java/com/targetmol/account/controller/InvoiceInfoController.java
+++ account/src/main/java/com/targetmol/account/controller/InvoiceInfoController.java
@@ -48,6 +48,8 @@ public class InvoiceInfoController {
     @DeleteMapping("{invoiceid}")
     public ResponseEntity<ResultMsg> delInvoiceInfo(@PathVariable("invoiceid") Integer invoiceid){
         invoiceInfoService.delInvoiceInfo(invoiceid);
+
         return ResponseEntity.ok(ResultMsg.success());
+
     }
 }
